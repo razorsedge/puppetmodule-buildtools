f10e3df25e54b1c42a42fe263c45cae33fc787ae
Michael Arnold <github@razorsedge.org>
2014-08-24 21:57:42 -0700

    Deal with the new metadata.json in Puppet 3.3.0.
    Also make sure to build from the master branch.

--- release.sh
+++ release.sh
@@ -9,11 +9,20 @@ rake spec_clean 2>/dev/null
 git flow release start $TAG || exit 2
 sed -i "s|^version .*|version '${TAG}'|" Modulefile
 git add Modulefile
+if [ -f .metadata.json ]; then
+  cp -p .metadata.json metadata.json
+fi
+#if [ -f metadata.json ]; then
+#  sed -i "/\"version\":/s|: \".*|: \"${TAG}\",|" metadata.json
+#  git add metadata.json
+#fi
 git commit -m "Update versions for $TAG release."
 git flow release finish -m "Puppet Forge $TAG release." $TAG && \
+git checkout master && \
 git-log-to-changelog | tail -n+5 >CHANGELOG &&
-puppet module build
-rm -f CHANGELOG
+puppet module build && \
+rm -f CHANGELOG metadata.json
+git checkout develop
 
 echo "** Upload via browser to the Forge"
 echo git push origin develop
