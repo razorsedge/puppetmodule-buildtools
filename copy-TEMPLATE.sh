170bb53a0cfddbd429c7bb4421a30019d65792e3
Michael Arnold <github@razorsedge.org>
2015-01-06 07:37:58 -0700

    Update pre-commit script.
    And then switch to not using it.

--- copy-TEMPLATE.sh
+++ copy-TEMPLATE.sh
@@ -12,8 +12,10 @@ done
 
 cd ${MODULE}
 git init
-mv pre-commit .git/hooks/
-git add * .gitignore .gemfile .fixtures.yml .travis.yml
+#mv pre-commit .git/hooks/
+rm pre-commit
+ln -s ~/git/drwahl/puppet-git-hooks/pre-commit .git/hooks/pre-commit
+git add * .gitignore .fixtures.yml .travis.yml .project
 git commit -m 'first commit'
 git tag 0.0.0
 git remote add origin git@github.com:razorsedge/puppet-${MODULE}.git
