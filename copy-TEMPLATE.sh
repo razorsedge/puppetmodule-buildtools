b280afcd81ebcf93344fc5cc796be8e68db2fa74
Michael Arnold <github@razorsedge.org>
2015-04-12 23:28:25 -0700

    Add .puppet-lint.rc. Do not include Gemfile.lock.

--- copy-TEMPLATE.sh
+++ copy-TEMPLATE.sh
@@ -12,10 +12,12 @@ done
 
 cd ${MODULE}
 git init
+mv Gemfile.lock .Gemfile.lock
 #mv pre-commit .git/hooks/
 rm pre-commit
 ln -s ~/git/drwahl/puppet-git-hooks/pre-commit .git/hooks/pre-commit
-git add * .gitignore .fixtures.yml .travis.yml .project
+git add * .gitignore .fixtures.yml .travis.yml .project .puppet-lint.rc
+mv .Gemfile.lock Gemfile.lock
 git commit -m 'first commit'
 git tag 0.0.0
 git remote add origin git@github.com:razorsedge/puppet-${MODULE}.git
